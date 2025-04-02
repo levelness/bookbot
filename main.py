9a6c7ff910b1fd05c84152e1c2a988a6b4f0d65e
Artyom Nazarov <anazarov@ivi.ru>
2025-04-02 08:22:05 +0300

    refactor stats to class

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import get_num_words, get_num_characters, get_sorted_characters
+from stats import Stats
 from sys import argv, exit
 
 def get_book_text(filepath):
@@ -18,9 +18,10 @@ def main():
     print(f"Analyzing book found at {path}...")
 
     text = get_book_text(path)
-    num = get_num_words(text)
-    characters = get_num_characters(text)
-    sorted_characters = get_sorted_characters(characters)
+    stats = Stats()
+    num = stats.get_num_words(text)
+    characters = stats.get_num_characters(text)
+    sorted_characters = stats.get_sorted_characters(characters)
 
     print("----------- Word Count ----------")
     print(f"Found {num} total words")
